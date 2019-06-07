15db980e6dac0079b1efc188f4c9812d9cd7b109
Darren Pham <phamdarren1@gmail.com>
2019-06-07 01:47:35 -0700

    staricase code complete

--- challenges/staircase.py
+++ challenges/staircase.py
@@ -14,9 +14,8 @@ import sys
 # Complete the staircase function below.
 def staircase(n):
 
+    for i in range(1,n+1):
+        print((' '*(n-i)) + ('#'*i))
     
-
 if __name__ == '__main__':
-    n = int(input())
-
-    staircase(n)
+    staircase(8)

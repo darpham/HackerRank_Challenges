d5b0a0742dd04e8bfcab5a8aadf15bc24fff9ebe
Darren Pham <phamdarren1@gmail.com>
2019-06-07 00:59:47 -0700

    staricase code

--- challenges/staircase.py
+++ challenges/staircase.py
@@ -3,3 +3,20 @@ https://www.hackerrank.com/challenges/staircase/problem?h_r=next-challenge&h_v=z
 '''
 
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+
+    
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
